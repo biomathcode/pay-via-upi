62129fc94ef83794f745e294d4836b39487547e9
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-05-05 12:02:56 +0530

    remove data not defined error

--- pages/preview.js
+++ pages/preview.js
@@ -1,27 +1,31 @@
 import Script from "next/script";
 import { useRecoilValue } from "recoil";
+import { scriptAtom } from "../stores/Store";
 
 function Preview() {
-
-   
+  const data = useRecoilValue(scriptAtom);
 
     return ( 
         <div>
-               <Script src='static/libs/main.js'
-      async
-    data-name="pay-via-upi"
-    data-cfasync="false"
-    data-pa={data.upi_id}
-    data-tn=""
-    data-cu="INR"
-    data-pn="Pratik Sharma"
-    data-amount_list = "100, 500, 1000, 2000"
-    data-label = "Donate via UPI"
-    data-description="Scan and Pay using UPI!"
-    data-color="#000"
-    data-position="Right"
-  
-  strategy="beforeInteractive" ></Script>
+          {data &&
+          <Script src='static/libs/main.js'
+          async
+        data-name="pay-via-upi"
+        data-cfasync="false"
+        data-pa={data.upi_id}
+        data-tn=""
+        data-cu="INR"
+        data-pn={data.name}
+        data-amount_list = "100, 500, 1000, 2000"
+        data-label = "Donate via UPI"
+        data-description="Scan and Pay using UPI!"
+        data-color="#000"
+        data-position="Right"
+      
+      strategy="afterInteractive" ></Script>
+          
+          }
+               
         </div>
      );
         
